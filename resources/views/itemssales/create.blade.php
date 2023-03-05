25c4b1f144914f5aaa094f64bb2d45a85a80b31b
dineshtkp <dineshtkp14@gmail.com>
2023-03-05 20:33:39 +0545

    ok

--- resources/views/itemssales/create.blade.php
+++ resources/views/itemssales/create.blade.php
@@ -14,64 +14,115 @@
     </div>
 
     <div class="container">
-
-        <a href="/daybooks/">Back</a>
+        <div class="mb-4">
+        <a class="d-block" href="/daybooks/">Back</a>
+        </div>
+      
 
         <form class="row gx-5 gy-3" action="{{ route('itemsales.store') }}" method="post">
             @csrf
-            <div class="col-md-12">
-                <label for="inputPassword4" class="form-label"> CustomerID</label>
-                <input type="text" class="form-control @error('customerid') is-invalid @enderror" name="customerid"
-                    value="{{ old('customerid') }}">
-                @error('customerid')
-                    <p class="invalid-feedback">{{ $message }}</p>
-                @enderror
+            <div class="row">
+            <table class="invoicetable table-responsive">
+                <tbody style="max-height: none;">
+                    <tr>
+                        <th>
+                            <button class=" btn btn-success">+</button>
+                        </th>
+                        <th class="w-25"> Product</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Discount</th>
+                        <th>Subtotal</th>
+                    </tr>
+                    <tr>
+                        <td><button class=" btn btn-danger">X</button></td>
+                        <td>
+                            <input type="text" class= "w-100 inputwidth form-control @error('itemid') is-invalid @enderror"
+                                name="itemid" value="{{ old('itemid') }}" placeholder="Enter a Product">
+                            @error('itemid')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror <a href="">or Select a Product</a>
+
+                        </td>
+
+                        <td>
+                            <input type="text" placeholder="Quantity"
+                                class="form-control @error('itemid') is-invalid @enderror" name="itemid"
+                                value="{{ old('itemid') }}">
+                            @error('itemid')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </td>
+                        <td>
+                            <div class="input-group">
+                                <span class="input-group-text" id="basic-addon1">$</span>
+                                <input type="text" placeholder="Price"
+                                    class="form-control @error('itemid') is-invalid @enderror" name="itemid"
+                                    value="{{ old('itemid') }}">
+                                @error('itemid')
+                                    <p class="invalid-feedback">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Discount"
+                                class="form-control @error('itemid') is-invalid @enderror" name="itemid"
+                                value="{{ old('itemid') }}">
+                            @error('itemid')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </td>
+                        <td>
+                            <div class="input-group">
+                                <span class="input-group-text" id="basic-addon1">$</span>
+                                <input type="text" placeholder="Sub-Total"
+                                    class="form-control @error('itemid') is-invalid @enderror" name="itemid"
+                                    value="{{ old('itemid') }}">
+                                @error('itemid')
+                                    <p class="invalid-feedback">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </td>
+                    </tr>
+                    
+                   
+                </tbody>
+            </table>
             </div>
 
-            <div class="col-md-12">
-                <label for="inputPassword4" class="form-label">ITEMID</label>
-                <input type="text" class="form-control @error('itemid') is-invalid @enderror" name="itemid"
-                    value="{{ old('itemid') }}">
-                @error('itemid')
-                    <p class="invalid-feedback">{{ $message }}</p>
-                @enderror
+            <br><br>
+            <br><br>
+
+            <div class="row my-5 p-0">
+                <div class="col-md-9">
+                    <div class="">
+                        <label class="my-3"><b>Amount in words: </b>Three thousand four hundred fifty-six
+                            fifty-sixhundred fifty-six fifty-six</label><br>
+                        <textarea placeholder="Additional notes" class="form-control" rows="3" cols="20"></textarea>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="">
+                        <div class="input-group mb-1">
+                            <span class="input-group-text" id="basic-addon1">Sub-Total</span>
+                            <input type="text" class="form-control" placeholder="Sub-Total" aria-label="Username"
+                                aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">Discount</span>
+                            <input type="text" class="form-control" placeholder="Sub-Total" aria-label="Username"
+                                aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-text" id="basic-addon1">Total</span>
+                            <input type="text" class="form-control" placeholder="" aria-label="Username"
+                                aria-describedby="basic-addon1">
+                        </div>
+                        <br>
+                        <button class="btn btn-success btn-lg">Save & Print</button>
+                        <button class="btn btn-success btn-lg">Save</button>
+                    </div>
+                </div>
             </div>
-
-            <div class="row gx-5 gy-3 form-wrapper" id="formWrapper"></div>
-
-            <button class="btn btn-info" id="addFieldBtn">+</button>
-
-            <div class="col-md-6">
-                <label for="inputPassword4" class="form-label">Sub-Total</label>
-                <input type="text" class="form-control @error('subtotalf') is-invalid @enderror" name="subtotalf"
-                    value="{{ old('subtotalf') }}">
-                @error('remarks')
-                    <p class="invalid-feedback">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="col-md-6">
-                <label for="inputPassword4" class="form-label">Discount</label>
-                <input type="text" class="form-control @error('discountf') is-invalid @enderror" name="discountf"
-                    value="{{ old('discountf') }}">
-                @error('discountf')
-                    <p class="invalid-feedback">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="col-md-6">
-                <label for="inputPassword4" class="form-label">Total</label>
-                <input type="text" class="form-control @error('total') is-invalid @enderror" name="total"
-                    value="{{ old('total') }}">
-                @error('total')
-                    <p class="invalid-feedback">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="d-grid gap-2 pt-2 pb-4">
-                <button type="submit" class="btn btn-lg btn-primary">Save</button>
-            </div>
-        </form>
-
     </div>
 @stop
