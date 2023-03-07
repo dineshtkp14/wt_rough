fb919927f2cc0a06f27b18b524e822630a434c21
dineshtkp <dineshtkp14@gmail.com>
2023-03-07 22:27:22 +0545

    ok

--- app/Http/Controllers/ItemsalesController.php
+++ app/Http/Controllers/ItemsalesController.php
@@ -30,50 +30,29 @@ class ItemsalesController extends Controller
 
     public function store(Request $req)
     {
-        $sales_arr = json_decode($req->sales_arr);
-        $final_arr = json_decode($req->final_arr);
+        $sales_arr = json_decode($req->sales_arr);//rowdetails
+        //dd($sales_arr[0]);
 
+        $final_arr = json_decode($req->final_arr);//finaltotal
+      
         // invoice insert
 
-<<<<<<< HEAD
-    //create invoice  retirns invoiceid
-    //calculate validateallitems from frontend and backed data
-    //insert all items in itemsalestable with the returned invoice id
-    //update invoice with validated calutaion 
-    //create new invoice histry ,billno,date,paidamount,dueamount,totalamount
-
-        
-        $disinfoobj=new salesitem();
-        $disinfoobj->customerid=$req->customerid;
-        $disinfoobj->itemid=$req->itemid;
-        $disinfoobj->unstockedname=$req->unstockedname;
-        $disinfoobj->quantity=$req->quantity;
-        $disinfoobj->price=$req->price;
-        $disinfoobj->discount=$req->discount;
-        $disinfoobj->subtotal=$req->subtotal;
-
-        $disinfoobj->subtotalf=$req->subtotalf;
-
-        $disinfoobj->discountf=$req->discountf;
-        $disinfoobj->total=$req->total;
-       
-        $disinfoobj->save();
-
-    
-
-        return redirect()->route('itemsales.create')->with('success','Items Added Sucessfully !!');  
-    
-    else{
-       
-        return redirect()->route('itemsales.create')->withErrors($validator)->withInput();
-
-=======
         // sales insert
         foreach ($sales_arr as $value) {
-            $data = new itemsale();
-            $data->quantity = $value->price;
+            $data = new salesitem();
+            $data->invoiceid = 1;
+            $data->itemid = $value->product;
+            $data->unstockedname = "kkk";
+
+            $data->quantity = $value->quantity;
+            $data->price = $value->price;
+            $data->discount = $value->discount;  
+            $data->subtotal = $value->subtotal;   
+
+            
+
             $data->save();
         }
->>>>>>> 44cff8e75a41c82889b9bda50d2caff09c205742
     }
 }
+
