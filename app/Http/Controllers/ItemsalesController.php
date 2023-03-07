e18a332e70e45ca6d404d40a0187fc981eb51642
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2023-03-07 21:24:28 +0545

    jquery sales item work

--- app/Http/Controllers/ItemsalesController.php
+++ app/Http/Controllers/ItemsalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\itemsale;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Session;
@@ -29,5 +30,16 @@ class ItemsalesController extends Controller
 
     public function store(Request $req)
     {
+        $sales_arr = json_decode($req->sales_arr);
+        $final_arr = json_decode($req->final_arr);
+
+        // invoice insert
+
+        // sales insert
+        foreach ($req->values as $value) {
+            $data = new itemsale();
+            $data->quantity = $value->price;
+            $data->save();
+        }
     }
 }
