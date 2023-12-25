4eef40d020f9a8a6e9cbad2697c4d7e3fef01e3c
dineshtkp <dineshtkp14@gmail.com>
2023-12-25 20:24:42 +0545

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,12 +11,14 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
+
     {
+        \App\Models\User::truncate();
         \App\Models\User::factory(1)->create([
             'name' => 'Dinesh Bajgain',
             'phoneno' => '1234567890',
             'email' => 'dineshtkp14@gmail.com',
-            'password' => bcrypt('nepal123'),
+            'password' => bcrypt('nepal12345'),
         ]);
     }
 }
