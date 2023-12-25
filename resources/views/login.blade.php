fbf706b97469d569bcad9d70655be301af059524
dineshtkp <dineshtkp14@gmail.com>
2023-12-25 19:59:30 +0545

    ok

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -33,7 +33,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand text-warning" href="">KPS</a>
+                <a class="navbar-brand text-warning" href="">wholesale Tikapur</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
