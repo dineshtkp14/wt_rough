909558de224e705ad438300634ec9ac36f39b732
dineshtkp <dineshtkp14@gmail.com>
2024-01-02 20:29:22 +0545

    update

--- resources/views/livewire/allcustomercreditlistlivewire.blade.php
+++ resources/views/livewire/allcustomercreditlistlivewire.blade.php
@@ -2,8 +2,8 @@
 	<div class="card">
 		<div class="card-header">
             <a href="{{route('companys.create')}}"><img src="https://img.icons8.com/glyph-neue/50/40C057/plus-2-math.png"/></a>
-            Total Credit of all Customer:- <b>{{$total_debit}}</b>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Total Advance Payment:- <b>{{$total_credit}}</b>
+            Total Credit of all Customer:- <span class="h3"><b>{{$total_debit}}</b></span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Total Advance Payment:- <span class="h3"><b>{{$total_credit}}</b></span>
 
               <input type="text" class="form-control float-end  border-warning border border-5" placeholder="Search By Name" style="width: 250px;" wire:model="searchTerm" >
          </div>
