db17db7de827b53925a56c3f27b039647718fb7e
dineshtkp <dineshtkp14@gmail.com>
2024-01-02 20:20:32 +0545

    update

--- app/Http/Livewire/Allcustomercreditlistlivewire.php
+++ app/Http/Livewire/Allcustomercreditlistlivewire.php
@@ -43,7 +43,7 @@ class Allcustomercreditlistlivewire extends Component
         }
     
         // Paginate the results
-        $allResults = $query->paginate(20);
+        $allResults = $query->paginate(10);
     
         // Fetch additional data
         foreach ($allResults as $data) {
