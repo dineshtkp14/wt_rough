49fd03549703ac99a9eb3aaffd6aec4e499e39d0
dineshtkp <dineshtkp14@gmail.com>
2024-01-05 15:13:08 +0545

    update

--- app/Http/Controllers/Daybookcontroller.php
+++ app/Http/Controllers/Daybookcontroller.php
@@ -130,7 +130,7 @@ public function update($id, Request $req)
             return redirect()->route('daybooks.index')->with('success','Daybook Details Updated Successfully !!');
         } else {
             dd("this is errror");
-            return redirect()->route('daybooks.edit', ['daybooks' => $daybook])->withErrors($validator)->withInput();
+            return redirect()->route('daybooks.create', ['daybooks' => $daybook])->withErrors($validator)->withInput();
         }
     }
 
