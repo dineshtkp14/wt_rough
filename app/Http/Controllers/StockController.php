29281ceb917e31288f14a7c87c96c2f39b2ec3b8
dineshtkp <dineshtkp14@gmail.com>
2024-02-10 20:48:48 +0545

    updateE

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -24,7 +24,7 @@ class StockController extends Controller
             'link' => 'View Stocks'
         ];
 //TRUE
-        $iteamdata = Item::orderBy('id', 'DESC')->get();
+        $iteamdata = Items::orderBy('id', 'DESC')->get();
 
         // $iteamdata = Item::where('check_remove_ofs', '=', 0)
                
