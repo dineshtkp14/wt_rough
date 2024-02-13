08ffa39d26dab52a3e341eec85e0684be94ee397
dineshtkp <dineshtkp14@gmail.com>
2024-02-13 21:26:02 +0545

    updateE

--- app/Models/purchaseorder.php
+++ app/Models/purchaseorder.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class purchaseorder extends Model
 {
     use HasFactory;
-    protected $table = 'PurchaseOrders';
+    protected $table = 'purchaseorders';
 
 }
