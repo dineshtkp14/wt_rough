574520d1482729e25c45f4982cd9c5274768052e
dineshtkp <dineshtkp14@gmail.com>
2024-02-25 09:22:42 +0545

    updateE

--- database/migrations/2024_02_24_152852_create_trackinvoice_table.php
+++ database/migrations/2024_02_24_152852_create_trackinvoice_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->text('updated_by');
+            $table->text('notes');
             $table->timestamps();
         });
     }
