bc3e55506fb6f5b9f2b7792e7f6d63b5fd99fa0f
dineshtkp <dineshtkp14@gmail.com>
2024-02-25 09:50:19 +0545

    updateE

--- database/migrations/2024_02_24_152852_create_trackinvoice_table.php
+++ database/migrations/2024_02_24_152852_create_trackinvoice_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +22,12 @@ return new class extends Migration
             $table->text('updated_by');
             $table->text('notes');
             $table->timestamps();
+        });
+
+        // Make created_at and updated_at columns not nullable
+        Schema::table('trackinvoice', function (Blueprint $table) {
+            $table->timestamp('created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+            $table->timestamp('updated_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->change();
         });
     }
 
