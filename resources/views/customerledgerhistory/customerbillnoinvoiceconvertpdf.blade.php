8e8027c556edaa2b15a18f37aa8045ad0b027cba
dineshtkp <dineshtkp14@gmail.com>
2024-04-14 22:37:04 +0545

    ok

--- resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
+++ resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
@@ -10,73 +10,100 @@
             margin: 0 !important;
             padding: 0 !important;
         }
+
         * {
             margin-top: 0 !important; /* Set top margin to 0 for all elements */
         }
+
         .container {
             margin: 0 auto;
             padding: 20px;
             background-color: white;
         }
-        p{
-            font-size: 16px !important;;
+
+        @page {
+            size: A5 portrait;
+            margin: 20px; /* Set margin to 0 for the page */
+            padding: 10px !important;
         }
+
+        p {
+            font-size: 16px !important;
+        }
+
         .letterhead {
-            /* background-color: black; */
             color: black;
             padding: 20px;
             text-align: center;
         }
+
         .letterhead h1 {
             margin: 0;
             font-size: 30px;
             text-decoration: underline;
         }
+
         .address-info {
             text-align: center;
             margin-top: 20px;
         }
 
-        .firstdiv{
+        .firstdiv {
             float: right;
         }
-        .seconddiv{
+
+        .seconddiv {
             margin-top: -30px !important;
         }
+
         .address-info p {
             margin: 5px 0;
             font-size: 14px;
         }
+
         .invoice-info {
             margin-top: 20px;
         }
+
         .invoice-info p {
             margin: 5px 0;
             font-size: 16px;
         }
+
         table {
             width: 100%;
             border-collapse: collapse;
             margin-top: 20px;
-            font-size: 14px;
+            font-size: 22px; /* Increase font size for table */
         }
+
         th, td {
-            border: 1px solid #000; /* Set border color to black */
-            padding: 6px;
+            border: 1px solid #000;
+            padding: 2px; /* Increase padding for table cells */
+            text-align: center;
         }
+
         th {
-            background-color: white; /* Set background color to white */
+            background-color: white;
         }
+
         .text-right {
             text-align: right;
         }
+
         .notes {
             margin-top: 20px;
             max-height: 100px;
             overflow: hidden;
             font-size: 14px;
         }
-        .forbillandpan{
+
+        /* Set minimum height for table container */
+        /* .table-container {
+            min-height: 500px; /* Adjust this value as needed */
+        } */
+
+        .forbillandpan {
             margin-top: -70px !important;
         }
     </style>
@@ -104,9 +131,21 @@
             </div>
        
        <div class="forbillandpan">
-            Invoice No: {{$invoiceid}} <br>
-            PAN No. 601064191
-       </div>
+        <span style="font-size: 18px;"> INVOICE NO: </span><b>{{$invoiceid}} </b><br>
+
+
+        @if ($allinvoices != null)
+                @foreach($allinvoices as $i)
+                    @if ($i->total < 19900)
+                        <span style="font-size: 18px;">PAN N0. 601064191</span>
+                    @endif
+                @endforeach
+        @endif
+
+    
+           
+        </div>
+        <BR>-
           
             <div class="seconddiv"> 
               
@@ -132,133 +171,139 @@
         </div>
     </div>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Serial No.</th>
-                <th>ITEM ID</th>
-                <th>ITEM Name</th>
-              
-                <th>Quantity</th>
-                <th>Unit</th>
-                <th>Sold Price</th>
-                <th>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php $serialNo = 1; @endphp
-            @if ($allcusbyid != null)
-                @foreach($allcusbyid as $i)
-                    <tr>
-                        <td>{{ $serialNo++ }}</td>
-                        <td>{{$i->itemidorg}}</td>
+    <div class="table-container">
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ITEM ID</th>
+                            <th>ITEM Name</th>
                         
-                        <td>{{$i->itemid}}</td>
-                       
-                        <td>{{$i->quantity}}</td>
-                        <td>{{$i->unit}}</td>
-                        <td>{{$i->price}}</td>
-                        <td>{{$i->subtotal}}</td>
-                    </tr>
-                @endforeach
-            @endif
+                            <th>Quantity</th>
+                            <th>Unit</th>
+                            <th>Sold Price</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php $serialNo = 1; @endphp
+                        @if ($allcusbyid != null)
+                            @foreach($allcusbyid as $i)
+                                <tr>
+                                    <td>{{ $serialNo++ }}</td>
+                                    <td>{{$i->itemidorg}}</td>
+                                    
+                                    <td>{{$i->itemid}}</td>
+                                
+                                    <td>{{$i->quantity}}</td>
+                                    <td>{{$i->unit}}</td>
+                                    <td>{{$i->price}}</td>
+                                    <td>{{$i->subtotal}}</td>
+                                </tr>
+                            @endforeach
+                        @endif
 
-            @if ($allinvoices != null)
-                @foreach($allinvoices as $i)
-                    <tr>
-                        <td colspan="5"></td>
-                        <td class="text-right"><b>Sub-Total:</b></td>
-                        <td><b>{{$i->subtotal}}</b></td>
-                    </tr>
-                    <tr>
-                        <td colspan="5"></td>
-                        <td class="text-right"><b>Extra Discount:</b></td>
-                        <td><b>{{$i->discount}}</b></td>
-                    </tr>
-                    <tr>
-                        
-                        <td colspan="5" class="">Amount in Words: 
-                        @php
-                            function convertNumberToWords($num) {
-                                $ones = array(
-                                    "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-                                    "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
-                                );
-                                $tens = array(
-                                    "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-                                );
+                        @if ($allinvoices != null)
+                            @foreach($allinvoices as $i)
+                                <tr>
+                                    <td colspan="5"></td>
+                                    <td class="text-right"><b>Sub-Total:</b></td>
+                                    <td class="p-4"><b>{{$i->subtotal}}</b></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5">
+                                        <p style=" font-size: 14px; text-align: left;">#  Goods once sold won't be returned</p>
 
-                                if ($num == 0) {
-                                    return "Zero";
-                                }
+                                    </td>
+                                    <td class="text-right"><b>Extra Discount:</b></td>
+                                    <td><b>{{$i->discount}}</b></td>
+                                </tr>
+                                <tr>
+                                    
+                                    <td colspan="5" class="" style="font-size: 14px; text-align: left; margin-left: 2px;"><b>Amount in Words: </b>
+                                        @php
+                                        function convertNumberToWords($num) {
+                                            $ones = array(
+                                                "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                                                "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+                                            );
+                                            $tens = array(
+                                                "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+                                            );
 
-                                $words = "";
+                                            if ($num == 0) {
+                                                return "Zero";
+                                            }
 
-                                if ($num >= 10000000) {
-                                    $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
-                                    $num %= 10000000;
-                                }
+                                            $words = "";
 
-                                if ($num >= 100000) {
-                                    $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
-                                    $num %= 100000;
-                                }
+                                            if ($num >= 10000000) {
+                                                $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
+                                                $num %= 10000000;
+                                            }
 
-                                if ($num >= 1000) {
-                                    $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
-                                    $num %= 1000;
-                                }
+                                            if ($num >= 100000) {
+                                                $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
+                                                $num %= 100000;
+                                            }
 
-                                if ($num >= 100) {
-                                    $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
-                                    $num %= 100;
-                                }
+                                            if ($num >= 1000) {
+                                                $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
+                                                $num %= 1000;
+                                            }
 
-                                if ($num >= 20) {
-                                    $words .= $tens[floor($num / 10)] . " ";
-                                    $num %= 10;
-                                }
+                                            if ($num >= 100) {
+                                                $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
+                                                $num %= 100;
+                                            }
 
-                                if ($num > 0) {
-                                    $words .= $ones[$num] . " ";
-                                }
+                                            if ($num >= 20) {
+                                                $words .= $tens[floor($num / 10)] . " ";
+                                                $num %= 10;
+                                            }
 
-                                return $words;
-                            }
+                                            if ($num > 0) {
+                                                $words .= $ones[$num] . " ";
+                                            }
 
-                            // Retrieve the numerical value from your data
-                            $number = $i->total;
+                                            return $words;
+                                        }
 
-                            // Convert the numerical value to words
-                            $words = convertNumberToWords($number);
+                                        // Retrieve the numerical value from your data
+                                        $number = $i->total;
 
-                            echo $words;
-                        @endphp
-                        only/-
-                        </td>
-                        <td class="text-right"><b>Total Amount:</b></td>
-                        <td colspan="" id="totalAmountWords">{{$i->total}}</td>
+                                        // Convert the numerical value to words
+                                        $words = convertNumberToWords($number);
 
-                    </tr>
-                    
-                    <tr>
-                        <td colspan="7" class="notes"><b>Notes:</b> {{$i->notes}}</td>
-                    </tr>
-                @endforeach
+                                        echo $words;
+                                    @endphp
+                                    only/-
+                                    </td>
+                                    <td class="text-right"><b>Total Amount:</b></td>
+                                    <td colspan="" id="totalAmountWords">{{$i->total}}</td>
 
+                                </tr>
+                                
+                                <tr>
+
+                                    <td colspan="7" class="notes" style="text-align: left"><b>Notes:</b> {{$i->notes}}</td>
+                                </tr>
+                            @endforeach
+
+                            
+                        @endif
+                    </tbody>
+                </table>
                 
-            @endif
-        </tbody>
-    </table>
-
-
-    <br>@if ($allinvoices !=null)
+    </div>
+    <br>
+    @if ($allinvoices !=null)
     @foreach($allinvoices as $i)
       
       <p>Bill Created_by: {{$i->added_by}} </p>
     @endforeach
 @endif
-    <p style="margin-top: 20px; font-size: 14px; text-align: center;">Notes:  Goods once sold won't be returned</p>
 </div>
 
 <script>
