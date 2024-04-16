8e9048cd10699c3f71cd1bb3c054e43e839db541
dineshtkp <dineshtkp14@gmail.com>
2024-04-16 12:16:07 +0545

    ok

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -64,7 +64,7 @@
                 
                  <div class="search-box">
                     <input id="customerIdInput" name="companyid"   hidden>
-                    <input type="text" value= "{{ old('distributorname',$item->distributorname) }}"  class="search-input @error('companyid') is-invalid @enderror" placeholder="Search Company Name"
+                    <input type="text" value= "{{ old('distributorname',$item->companyid) }}"  class="search-input @error('companyid') is-invalid @enderror" placeholder="Search Company Name"
                                 id="searchCustomerInput"  data-api="company_search" autocomplete="off">
                                 @error('companyid')
                                 <p class="invalid-feedback m-0" style="position: absolute; bottom: -24px; left: 0;">{{ $message }}</p>
