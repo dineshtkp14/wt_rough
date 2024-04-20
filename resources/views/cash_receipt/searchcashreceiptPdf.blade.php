0abe888aced6f79b3c5972cd8fdaa4002fee4407
dineshtkp <dineshtkp14@gmail.com>
2024-04-20 13:21:41 +0545

    ok

--- resources/views/cash_receipt/searchcashreceiptPdf.blade.php
+++ resources/views/cash_receipt/searchcashreceiptPdf.blade.php
@@ -140,13 +140,62 @@
             <p><strong>Amount:</strong> {{$data->credit}}/-</p>
             <p><strong>Amount In Words:</strong>
                 @php
-                    // Function to convert number to words
-                    function convertNumberToWords($num) {
-                        // Your conversion logic here...
-                    }
-                    // Convert the amount to words
-                    echo convertNumberToWords($data->credit);
-                @endphp
+                                                        function convertNumberToWords($num) {
+                                                            $ones = array(
+                                                                "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                                                                "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+                                                            );
+                                                            $tens = array(
+                                                                "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+                                                            );
+
+                                                            if ($num == 0) {
+                                                                return "Zero";
+                                                            }
+
+                                                            $words = "";
+
+                                                            if ($num >= 10000000) {
+                                                                $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
+                                                                $num %= 10000000;
+                                                            }
+
+                                                            if ($num >= 100000) {
+                                                                $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
+                                                                $num %= 100000;
+                                                            }
+
+                                                            if ($num >= 1000) {
+                                                                $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
+                                                                $num %= 1000;
+                                                            }
+
+                                                            if ($num >= 100) {
+                                                                $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
+                                                                $num %= 100;
+                                                            }
+
+                                                            if ($num >= 20) {
+                                                                $words .= $tens[floor($num / 10)] . " ";
+                                                                $num %= 10;
+                                                            }
+
+                                                            if ($num > 0) {
+                                                                $words .= $ones[$num] . " ";
+                                                            }
+
+                                                            return $words;
+                                                        }
+
+                                                        // Retrieve the numerical value from your data
+                                                        $number = $data->credit ?? '' ;
+
+                                                        // Convert the numerical value to words
+                                                        $words = convertNumberToWords($number);
+
+                                                        echo $words;
+                                                    @endphp
+                                                    only/-
             </p>
             <p><strong>Notes:</strong> {{$data->notes}}</p>
         @endforeach
