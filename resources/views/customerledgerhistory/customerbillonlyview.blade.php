bc86afc6415913ba6acfb73ccea313ad40c40a56
dineshtkp <dineshtkp14@gmail.com>
2024-04-27 09:40:14 +0545

    ok

--- resources/views/customerledgerhistory/customerbillonlyview.blade.php
+++ resources/views/customerledgerhistory/customerbillonlyview.blade.php
@@ -236,66 +236,71 @@
                         <td colspan="6">
 
 
-                            function convertNumberToWords($num) {
-                                $ones = array(
-                                    "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-                                    "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
-                                );
-                                $tens = array(
-                                    "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-                                );
-                            
-                                if ($num == 0) {
-                                    return "Zero";
-                                }
-                            
-                                $words = "";
-                            
-                                if ($num < 20) {
-                                    // Handle numbers less than 20 separately
-                                    $words .= $ones[$num];
-                                } else {
-                                    if ($num >= 10000000) {
-                                        $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
-                                        $num %= 10000000;
-                                    }
-                            
-                                    if ($num >= 100000) {
-                                        $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
-                                        $num %= 100000;
-                                    }
-                            
-                                    if ($num >= 1000) {
-                                        $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
-                                        $num %= 1000;
-                                    }
-                            
-                                    if ($num >= 100) {
-                                        $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
-                                        $num %= 100;
-                                    }
-                            
-                                    if ($num >= 20) {
-                                        $words .= $tens[floor($num / 10)] . " ";
-                                        $num %= 10;
-                                    }
-                            
-                                    if ($num > 0) {
-                                        $words .= $ones[$num] . " ";
-                                    }
-                                }
-                            
-                                return $words;
-                            }
-                            
-                            // Retrieve the numerical value from your data
-                            $number = $i->total;
-                            
-                            // Convert the numerical value to words
-                            $words = convertNumberToWords($number);
-                            
-                            echo $words;
-                            
+                            @php
+                        
+                        function convertNumberToWords($num) {
+    $ones = array(
+        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    );
+    $tens = array(
+        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    );
+
+    if ($num == 0) {
+        return "Zero";
+    }
+
+    $words = "";
+
+    if ($num < 20) {
+        // Handle numbers less than 20 separately
+        $words .= $ones[$num];
+    } else {
+        if ($num >= 10000000) {
+            $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
+            $num %= 10000000;
+        }
+
+        if ($num >= 100000) {
+            $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
+            $num %= 100000;
+        }
+
+        if ($num >= 1000) {
+            $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
+            $num %= 1000;
+        }
+
+        if ($num >= 100) {
+            $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
+            $num %= 100;
+        }
+
+        if ($num >= 20) {
+            $words .= $tens[floor($num / 10)] . " ";
+            $num %= 10;
+        }
+
+        if ($num > 0) {
+            $words .= $ones[$num] . " ";
+        }
+    }
+
+    return $words;
+}
+
+// Retrieve the numerical value from your data
+$number = $i->total;
+
+// Convert the numerical value to words
+$words = convertNumberToWords($number);
+
+echo $words;
+
+
+
+                        @endphp
                         only/-
 
                         </td>
