9207d4d1f5f11c1838a3039e58491eaf5bfd78c0
dineshtkp <dineshtkp14@gmail.com>
2025-03-20 07:51:37 +0545

    ok

--- resources/views/customerledgerhistory/view_customerallledger_cashandcredit.blade.php
+++ resources/views/customerledgerhistory/view_customerallledger_cashandcredit.blade.php
@@ -123,6 +123,7 @@
 			<h5>Address: {{$i->address}}</h5>
 			<h5>Phone No: {{$i->phoneno}} , {{$i->alternate_phoneno}}</h5>
 			<h5>Email: {{$i->email}}</h5>
+			<h5>Email: {{$i->notes}}</h5>
 
 		</div>
 	@endforeach
