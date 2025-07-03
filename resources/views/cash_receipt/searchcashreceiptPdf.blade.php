b0bb7c180ffc91cc5437a32521a362d7a2821813
dineshtkp <dineshtkp14@gmail.com>
2025-07-03 18:46:57 +0545

    ok

--- resources/views/cash_receipt/searchcashreceiptPdf.blade.php
+++ resources/views/cash_receipt/searchcashreceiptPdf.blade.php
@@ -245,7 +245,7 @@
             <span class="forunderline fw-bold ps-2">
                 {{-- {{ $dts - $cts }} -/ --}}
                 <span style="
-                font-size: 45px; font-weight: bold;
+                font-size: 55px; font-weight: bold;
                 
             ">
                 {{ number_format($dts - $cts, 2) }}
