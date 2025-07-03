1d0a367ae623cbe3f610541c48fceb49c1b092ad
dineshtkp <dineshtkp14@gmail.com>
2025-07-03 17:56:20 +0545

    ok

--- resources/views/cash_receipt/searchcashreceiptPdf.blade.php
+++ resources/views/cash_receipt/searchcashreceiptPdf.blade.php
@@ -241,10 +241,12 @@
 <p style="font-size: 14px !important; margin-top:50px;">Printed Time and Date: <span style="color: #4b4b4b; font-size: 14px;"><?php echo date("Y-m-d H:i:s"); ?></span></p>
 
         <div style="margin-top: 70px; background-color: black;color:white; border: 1px solid black; padding: 10px;">
-            Total Due Amount: 
-            <span class="forunderline fw-bold ps-4">
-                {{-- {{ $dts - $cts }} -/ -----}}
-               <b> {{ number_format($dts - $cts, 2) }} </b>  -/
+            Total Due Amount:::: 
+            <span class="forunderline fw-bold ps-2">
+               
+               {{-- <b> {{ number_format($dts - $cts, 2) }} </b>  -/ --}} 
+               <span style="font-size: 28px;"><b>{{ number_format($dts - $cts, 2) }}</b></span> -/
+
             </span>
             <span style="font-size: 16px;"> ( as of the date and time: <?php echo date("Y-m-d H:i:s"); ?>) </span>
         </div>
