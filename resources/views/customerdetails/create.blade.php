5b8e4e20a2a04561be96bb9a3d29186bc2cea5c1
dineshtkp <dineshtkp14@gmail.com>
2025-08-10 21:07:40 +0545

    ok

--- resources/views/customerdetails/create.blade.php
+++ resources/views/customerdetails/create.blade.php
@@ -106,14 +106,14 @@
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-2">
                 <div class="form-check d-flex align-items-center">
                     <input class="form-check-input me-2" type="checkbox" id="forautoinputcash" name="forautoinputcash" style="width: 30px; height: 30px;">
                     <label class="form-check-label" for="forautoinputcash">If Cash</label>
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="form-check d-flex align-items-center">
                     <input class="form-check-input me-2" type="checkbox" id="clearamount" name="clearamount" style="width: 30px; height: 30px;">
                     <label class="form-check-label" for="clearamount">If Cash</label>
