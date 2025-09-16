476a486ff567e332ce642a68dd229c0034d3929e
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 12:54:47 +0545

    ok

--- resources/views/customerledgerhistory/customersoldpricecheck.blade.php
+++ resources/views/customerledgerhistory/customersoldpricecheck.blade.php
@@ -151,76 +151,7 @@
 
 <BR>
 
-@if(auth()->check() && auth()->user()->email == 'dineshtkp14@gmail.com')
-    <h6 class="floatleft">Total Transaction Amount: <span class="forunderline">{{ $dts }} /-</span></h6>
-@endif
 
-
-(
-@php
-              function convertNumberToWords($num) {
-    $ones = array(
-        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
-    );
-    $tens = array(
-        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-    );
-
-    // Handle negative numbers
-    if ($num < 0) {
-        return "Minus " . convertNumberToWords(abs($num));
-    }
-
-    if ($num == 0) {
-        return "Zero";
-    }
-
-    $words = "";
-
-    if ($num >= 10000000) {
-        $words .= convertNumberToWords(floor($num / 10000000)) . " Crore ";
-        $num %= 10000000;
-    }
-
-    if ($num >= 100000) {
-        $words .= convertNumberToWords(floor($num / 100000)) . " Lakh ";
-        $num %= 100000;
-    }
-
-    if ($num >= 1000) {
-        $words .= convertNumberToWords(floor($num / 1000)) . " Thousand ";
-        $num %= 1000;
-    }
-
-    if ($num >= 100) {
-        $words .= convertNumberToWords(floor($num / 100)) . " Hundred ";
-        $num %= 100;
-    }
-
-    if ($num >= 20) {
-        $words .= $tens[floor($num / 10)] . " ";
-        $num %= 10;
-    }
-
-    if ($num > 0) {
-        $words .= $ones[$num] . " ";
-    }
-
-    return $words;
-}
-
-// Retrieve the numerical value from your data
-$number = $allnotcash - $cts;
-// Convert the numerical value to words
-$words = convertNumberToWords($number);
-
-echo $words;
-
-            @endphp
-			
-			
-			)
 
 			{{$all->links()}}
 
