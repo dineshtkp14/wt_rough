70479876e78938ff0932e549eedb256c3dfac503
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 15:40:56 +0545

    ok

--- resources/views/customerledgerhistory/customersoldpricecheck.blade.php
+++ resources/views/customerledgerhistory/customersoldpricecheck.blade.php
@@ -67,12 +67,12 @@
         <div class="col-md-6">
             @foreach ($cusinfoforpdfok as $i)
               <div class="mb-3">
-                <div>CUSTOMER ID: <span class="fw-semibold">{{ $i->id }}</span></div>
+                {{-- <div>CUSTOMER ID: <span class="fw-semibold">{{ $i->id }}</span></div> --}}
                 <div>NAME: <span class="fw-semibold">{{ $i->name }}</span></div>
                 <div>ADDRESS: <span class="fw-semibold">{{ $i->address }}</span></div>
                 <div>PHONE NO: <span class="fw-semibold">{{ $i->phoneno }}, {{ $i->alternate_phoneno }}</span></div>
-                <div>EMAIL: <span class="fw-semibold">{{ $i->email }}</span></div>
-                <div>NOTES: <span class="fw-semibold">{{ $i->remarks }}</span></div>
+                {{-- <div>EMAIL: <span class="fw-semibold">{{ $i->email }}</span></div>
+                <div>NOTES: <span class="fw-semibold">{{ $i->remarks }}</span></div> --}}
               </div>
             @endforeach
           </div>
