2fcefc266d6d2c7e30d1048b9961ec722d689a19
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 11:07:09 +0545

    ok

--- app/Http/Controllers/CustomerLedgerHistroy.php
+++ app/Http/Controllers/CustomerLedgerHistroy.php
@@ -1070,6 +1070,7 @@ public function oldpricecheck(Request $req)
             'customeridfor'  => $customeridfor,
             'cusinfoforcard' => $cusinfoforcard,
             'cus'            => $cus,
+            'cusinfoforpdfok'   => $cusinfoforpdfok,
         ]);
     }
 
