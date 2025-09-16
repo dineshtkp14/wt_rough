de5508dbe8a27f66c913329bcfcb3a9caa59d557
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 16:25:57 +0545

    ok

--- resources/views/customerledgerhistory/_items_block.blade.php
+++ resources/views/customerledgerhistory/_items_block.blade.php
@@ -25,7 +25,7 @@
                 <td>{{ $item->unstockedname ?: '-' }}</td>
                 <td>{{ $item->quantity }}-{{ $item->unit }}</td>
                 <td>{{ $item->itemdlp }}</td>
-                <td ><button class="btn btn-primary">{{ $item->price }}</button></td>
+                <td ><button class="btn btn-dark">{{ $item->price }}</button></td>
               </tr>
             @endforeach
           @else
