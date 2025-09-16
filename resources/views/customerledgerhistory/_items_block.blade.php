6f7ed43c9b5f6667e0a11ecb8046937a1b8aba05
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 15:06:35 +0545

    ok

--- resources/views/customerledgerhistory/_items_block.blade.php
+++ resources/views/customerledgerhistory/_items_block.blade.php
@@ -4,18 +4,16 @@
         <thead>
           <tr>
             <th>Date</th>
-            <th>Created at</th>
+           
             <th>Bill No</th>
             <th>Name</th>
-            <th>Invoice Type</th>
+          
             <th>Items Name</th>
             <th>Unstocked Name</th>
             <th>Quantity</th>
             <th>Cost Price</th>
-            <th>Original Sell Price</th>
-            <th>Sold Price</th>
-            <th>Sub-Total</th>
-            <th>Profit</th>
+           
+           
           </tr>
         </thead>
         <tbody>
@@ -23,18 +21,15 @@
             @foreach ($cus as $item)
               <tr @if (date('Y-m-d', strtotime($item->date)) === date('Y-m-d')) style="font-weight:bold;color:white;background:red;" @endif>
                 <td>{{ $item->date }}</td>
-                <td>{{ $item->created_at }}</td>
+               
                 <td>{{ $item->invoiceid }}</td>
                 <td>{{ $item->customername }}</td>
-                <td>{{ $item->inv_type }}</td>
+                
                 <td>{{ $item->itemname ?: '-' }}</td>
                 <td>{{ $item->unstockedname ?: '-' }}</td>
                 <td>{{ $item->quantity }}</td>
                 <td>{{ $item->itemdlp }}</td>
-                <td>{{ $item->itemprice ?: '-' }}</td>
                 <td>{{ $item->price }}</td>
-                <td>{{ $item->subtotal }}</td>
-                <td>{{ !empty($item->itemdlp) ? ($item->price - $item->itemdlp) * $item->quantity : '-' }}</td>
               </tr>
             @endforeach
           @else
