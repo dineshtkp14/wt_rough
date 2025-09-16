2c2958574ab54fedfd436956848f2aafbe9d5cc5
dineshtkp <dineshtkp14@gmail.com>
2025-09-16 16:28:44 +0545

    ok

--- app/Http/Controllers/CustomerLedgerHistroy.php
+++ app/Http/Controllers/CustomerLedgerHistroy.php
@@ -1001,7 +1001,7 @@ public function oldpricecheck(Request $req)
     if (!Auth::check()) return redirect()->route('login');
 
     $breadcrumb = [
-        
+        'subtitle' => '',
         'title'    => 'Check Old Price',
         'link'     => 'Check Old Price',
     ];
