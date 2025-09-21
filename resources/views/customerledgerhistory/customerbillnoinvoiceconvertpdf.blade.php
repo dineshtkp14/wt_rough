fe122dd84b45e27f9000523bd61ba6f006bf4d9b
dineshtkp <dineshtkp14@gmail.com>
2025-09-21 10:17:24 +0545

    ok

--- resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
+++ resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
@@ -168,9 +168,10 @@
                     @endif
                     <p>Date: {{ $forinvoicetype->date }}</p>
 
-                    <p class="nep nepfix">
-                        मिति   : {{ \App\Support\NepaliDate::adToBsString($forinvoicetype->date ?? now()->toDateString(), 'np') }}
-                      </p>             
+                    <p style="font-family:'NotoSansDevanagari' !important; display:inline-block; letter-spacing:0;">
+                        &#8203;मिति: {{ \App\Support\NepaliDate::adToBsString($forinvoicetype->date ?? now()->toDateString(), 'np') }}
+                      </p>
+                                
                        @endif
             
             </div>
