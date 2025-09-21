d878f5ca09cab910b3ad080e1095ca7abb4aff46
dineshtkp <dineshtkp14@gmail.com>
2025-09-21 17:11:41 +0545

    ok

--- resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
+++ resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
@@ -47,9 +47,7 @@
     .nep, .label-nep{ font-family:'HindDevanagari',sans-serif; line-height:1.14; }
     .label-nep{ display:inline-block; padding-left:3px; font-size: 18px; } /* avoids matra clipping */
 
-    .forfontdate{
-        font-size: 18px;
-    }
+   
     /* INVOICE NO / PAN block */
     .forbillandpan{ margin-top:-80px !important; line-height:1.12; }
     .invoice-no{ font-size:18px; font-weight:700; letter-spacing:.3px; margin-bottom:1px; }
