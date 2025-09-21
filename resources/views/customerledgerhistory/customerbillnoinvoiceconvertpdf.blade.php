a99cc344e1952120fadc6c09a293428460845f43
dineshtkp <dineshtkp14@gmail.com>
2025-09-21 08:21:00 +0545

    ok

--- resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
+++ resources/views/customerledgerhistory/customerbillnoinvoiceconvertpdf.blade.php
@@ -148,7 +148,7 @@
                         <p>Invoice Type: {{ $forinvoicetype->invoicetype }}</p>
                     @endif
                     <p>Datee: {{ $forinvoicetype->date }}</p>
-                    <td>{{ \App\Support\NepaliDate::adToBsString($forinvoicetype->date, 'np') }}</td>
+                    <p>{{ \App\Support\NepaliDate::adToBsString($forinvoicetype->date, 'np') }}</p>
 
                 @endif
             
