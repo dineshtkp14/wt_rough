ad0d8a56ede37038ef5192d938007bf38f1587e2
dineshtkp <dineshtkp14@gmail.com>
2025-09-22 08:27:20 +0545

    ok

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,7 +62,7 @@ html body{
       font-size:18px; line-height:1.12;
 }
 .nep, .label-nep{ font-family:'HindDevanagari',sans-serif; line-height:1.14; }
-    .label-nep{ display:inline-block; padding-left:3px; } /* avoids matra clipping */
+    .label-nep{ padding-left:3px; } /* avoids matra clipping */
 </style>
 
 </head>
