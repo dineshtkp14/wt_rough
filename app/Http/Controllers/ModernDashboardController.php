e7aca8300ce5d93cfe29b3c326d8395f7c268a24
dineshtkp <dineshtkp14@gmail.com>
2026-05-03 09:00:00 +0545

    ok

--- app/Http/Controllers/ModernDashboardController.php
+++ app/Http/Controllers/ModernDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\item;
 use App\Models\customerinfo;
