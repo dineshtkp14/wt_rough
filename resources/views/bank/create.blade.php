1c738d8e51f053afe89a0d6174bd9e2fbc961732
dineshtkp <dineshtkp14@gmail.com>
2026-09-04 22:03:50 +0545

    ok

--- resources/views/bank/create.blade.php
+++ resources/views/bank/create.blade.php
@@ -63,7 +63,7 @@
             </div>
 
             <div class="d-grid gap-2 pt-2 pb-4">
-                    <button type="submit" id="submitBtn" class="btn btn-lg btn-primary">Save</button>
+                    <button type="submit" id="saveBankBtn" class="btn btn-lg btn-primary">Save</button>
             </div>
 </form>
 </div>
@@ -76,7 +76,7 @@
     $(document).ready(function () {
             $('form').submit(function () {
                 // Disable the submit button
-                $('#submitBtn').prop('disabled', true);
+                $('#saveBankBtn').prop('disabled', true);
                 
             });
         });
@@ -84,4 +84,3 @@
         </script>
 
 @stop
-
